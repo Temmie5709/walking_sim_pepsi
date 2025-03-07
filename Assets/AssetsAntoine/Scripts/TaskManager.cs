80092bbaac8d2c989ad4a6d3360612ddd32c650c
Antoine <161435725+MrNegetZ@users.noreply.github.com>
2025-03-07 12:34:25 +0100

    Finalisation de TaskManager pour gérer les taches
    Task Manager devra être appelé par les objets qui suppriment ou ajoutent des taches.
    Chaque tache aura un ID et un Texte.
    Pour supprimer une tache il faut l'ID!

--- Assets/AssetsAntoine/Scripts/TaskManager.cs
+++ Assets/AssetsAntoine/Scripts/TaskManager.cs
@@ -1,27 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TaskManager : MonoBehaviour
 {
 
-    
-    public class ActiveTasks{
-        public string Name;
-        public int Count;
+    public class Task
+    {
+        public string Text;
+        public int ID;
         private GameObject TaskObject;
+
+        public Task(string text, int id, GameObject TaskPrefab)
+        {
+            ID = id;
+            Text = text;
+            TaskObject = Instantiate(TaskPrefab);
+            TaskObject.GetComponent<TextMeshProUGUI>().SetText(text);
+        }
+
+       public void Delete()
+       {
+            GameObject.Destroy(TaskObject);
+       }
     }
 
-    
 
-    void Start()
+    public List<Task> ActiveTasks = new List<Task>();
+    [SerializeField] GameObject TaskPrefab;
+
+    public void CreateTask(string text, int id)
     {
-        
+        ActiveTasks.Add(new Task(text, id, TaskPrefab));
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StopTask(int id)
     {
-        
+       Task Tasktodelete = ActiveTasks[id];
+        Tasktodelete.Delete();
     }
 }
